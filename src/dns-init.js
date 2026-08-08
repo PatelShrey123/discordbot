25d07d2a1ce03f6a4d7059189fab85499c89fa95
PatelShrey123 <meetshrey2402@gmail.com>
2026-08-08 16:01:46 +0530

    chore: log Node.js version on startup

--- src/dns-init.js
+++ src/dns-init.js
@@ -1,3 +1,4 @@
 import dns from 'dns';
 dns.setDefaultResultOrder('ipv4first');
 console.log('🌐 DNS resolution order set to ipv4first');
+console.log(`🟢 Running on Node.js version: ${process.version}`);
